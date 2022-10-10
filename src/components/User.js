21323dd69839cab640a05856cf76af0b5f6bdc15
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-10-10 22:28:57 +0100

    style: creation du style pour le composant user.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,18 @@
 import "../styles/user.css"
+import profil from '../media/profil.jpg'
+import {TbMessageCircle} from'react-icons/tb'
 
 export default function User(){
     return(
         <div  className="userPage">
-           user
+            <img  className="profil" src={profil}  alt="profil" />
+            <div className="divPrincipal">
+                <div containerMsg>
+                     <TbMessageCircle className="msgCircle" />
+                </div>
+                <div className="barjaune">
+                </div>
+            </div>
         </div>
     )
 }
