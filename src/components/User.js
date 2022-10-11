8e3144ccac09ee4349f17325aca670fee124dc30
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-10-11 23:39:40 +0100

    styles: style pour le composant DisplayUsers

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,14 @@
 import "../styles/user.css"
 import profil from '../media/profil.jpg'
-import {TbMessageCircle} from'react-icons/tb'
+import {AiFillMessage} from'react-icons/ai'
 
 export default function User(){
     return(
         <div  className="userPage">
             <img  className="profil" src={profil}  alt="profil" />
             <div className="divPrincipal">
-                <div containerMsg>
-                     <TbMessageCircle className="msgCircle" />
+                <div className="containerMsg">
+                     <AiFillMessage className="msgCircle" />
                 </div>
                 <div className="barjaune">
                 </div>
