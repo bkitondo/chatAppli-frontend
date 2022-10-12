8fb61d8774a5a5717eb8c397d8369c6419163720
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-10-12 14:29:10 +0100

    style:ajout du style pour la page de connexion

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
+import '../styles/Menu.css'
 
 const Menu =()=>{
     return(
-        <nav>
-            <Link  to= "/">
+        <nav >
+            <Link className="link" to= "/">
                 connexion
             </Link>
-            <Link  to= "/inscription">
+            <Link className="link" to= "/inscription">
                 inscription
             </Link>
         </nav>
