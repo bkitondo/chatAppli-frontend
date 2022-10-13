9ed9c5aea02465ec03a53a318829e3598553998d
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-10-13 09:51:31 +0100

    fix: recovery of the values entered by the user in the various fields of the registration form

--- src/components/Sign_in.jsx
+++ src/components/Sign_in.jsx
@@ -1,13 +1,40 @@
 import Menu from "./Menu"
 import '../styles/Login.css'
+import { useState } from "react"
 
 export default function SignIn(){
+    const [id,setId] = useState("")
+    const [password, setPassword] = useState("")
+          console.log(`ùùùùùùùùù ${id}`);
+          console.log(password);
+
+          function Submit(e){
+                    e.preventDefault()
+          }
     return(
         <main className="item">
-            <form action="">
-                <input className="champ" type="text" placeholder="Identifiant"/>
-                <input  className="champ" type="password" placeholder="Mot de passe" />
-                <input className="bouton" type="submit"  value="connexion"/>    
+            <form action="" >
+                <input  className="champ" 
+                        type="text" 
+                        placeholder="Identifiant" 
+                        name="Id" 
+                        onChange={(e)=> setId(e.target.value)} 
+                        value={id}
+                        required
+                />
+                <input  className="champ" 
+                        type="password" 
+                        placeholder="Mot de passe" 
+                        name="Password" 
+                        onChange={(e)=> setPassword(e.target.value)} 
+                        value={password} 
+                        required
+                />
+                <input  className="bouton" 
+                        type="submit"  
+                        value="connexion"
+                        onSubmit={Submit}
+                />    
             </form>
             <Menu/>
         </main>
--- src/components/Sign_up.jsx
+++ src/components/Sign_up.jsx
@@ -1,17 +1,55 @@
 import Menu from "./Menu"
 // import '../styles/SignIn_Up.css'
 import '../styles/Login.css'
+import { useState } from "react"
 
 export default function SignUp(){
+
+    const [id, setId] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirPassword] =useState("")
+    console.log(`votre id ${id} votre mail ${email}  votre password ${password} votre confirmpassword ${confirmPassword}`);
+
+    const Submit = (e)=>{
+            e.preventDefault()
+    }
+
     return(
        <main className="item">
             <form action="">
-                <input className="champ" type="text" placeholder="Identifiant" />
-                <input className="champ" type="email" placeholder="email" />
-                <input className="champ" type="password" placeholder="Mot de passe" />
-                <input className="champ" type="password" placeholder="Confirmez votre mot de passe" />
+                <input 
+                        className="champ" 
+                        type="text" 
+                        placeholder="Identifiant"
+                        onChange={(e)=>{setId(e.target.value)}} value={id} 
+                />
+                <input 
+                        className="champ" 
+                        type="email" 
+                        placeholder="email" 
+                        onChange={(e)=>{setEmail(e.target.value)}} value={email}
+                />
+                <input 
+                        className="champ" 
+                        type="password" 
+                        placeholder="Mot de passe" 
+                        onChange={(e)=>{setPassword(e.target.value)}} value={password}
+                />
+                <input 
+                        className="champ"  
+                        type="password" 
+                        placeholder="Confirmez votre mot de passe" 
+                        onChange={(e)=>{setConfirPassword(e.target.value)}} value={confirmPassword}
+                />
                 {/* <input  type="file"  /> */}
-                <input className="bouton" type="submit" value="inscription" />    
+                <input className="bouton" 
+                        type="submit" 
+                        value="inscription" 
+                       onSubmit={ ()=>
+                        !({password}==={confirmPassword})? console.log(` mot de passe different`): {Submit}
+                       } 
+                />    
             </form>
             <Menu/>
        </main>
