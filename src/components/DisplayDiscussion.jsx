5c6eba9ca1f78d79a9f3059048a55c5168e00ac3
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-10-21 14:35:23 +0100

    feat: send message

--- src/components/DisplayDiscussion.jsx
+++ src/components/DisplayDiscussion.jsx
@@ -5,19 +5,22 @@ import { useState } from "react"
 import axios from "axios"
 import { addMessageRoute} from '../utils/url'
 
-
-export default function DisplayDiscussion({recentUser}){
+export default function DisplayDiscussion({currentChat}){
     const [messageSended, setMessageSended] = useState("")
     const Message = ["salutt", "bonjour"]
     const currentUserId = localStorage.getItem("userId") 
-    // console.log(`le curentUserId ${currentUserId}`);
     
     const sendMsg = (e)=>{
             e.preventDefault()
             axios.post(addMessageRoute,{
                     message : messageSended,
-                    from : currentUserId
+                    from : currentUserId,
+                    to : currentChat.userId
             })
+            .then(() => {
+                setMessageSended("")
+                console.log("message envoyé avec succes");})
+            .catch((err) => console.log(err));
             // Message.push(messageSended)
             // console.log("Hello");
             console.log(`message ${messageSended}`);
@@ -26,8 +29,8 @@ export default function DisplayDiscussion({recentUser}){
     return(
         <div className="discussionPage">
            <ul   className="person">
-                <li className="userName" >{recentUser.userName}</li>
-                <li className="recentMessage">{recentUser.message}</li>
+                <li className="userName" >{currentChat.userName}</li>
+                <li className="recentMessage">{currentChat.message}</li>
            </ul>
            <div className="allMessages">
             {Message.map((message)=>(
@@ -38,7 +41,7 @@ export default function DisplayDiscussion({recentUser}){
                 </ul>
             ))}
            </div>
-           <form className="message" onSubmit={e => sendMsg(e)}>
+           <form className="message" onSubmit={sendMsg}>
                 <textarea onChange={(e)=> setMessageSended(e.target.value)} value={messageSended} rows="1" className="msg"/>
                 <button type="submit" className="btn">
                     <AiOutlineSend className="send" />
--- src/components/DisplayUsers.jsx
+++ src/components/DisplayUsers.jsx
@@ -3,28 +3,26 @@ import {RiSearchLine} from 'react-icons/ri'
 import {FiMoreVertical} from 'react-icons/fi'
 import photo from '../media/profil.jpg'
 import axios from 'axios'
-import { useEffect, useRef, useState } from 'react'
-import { signUpRoute, serverRoute} from '../utils/url'
+import { useEffect, useState } from 'react'
+import { signUpRoute} from '../utils/url'
 // import {io} from "socket.io-client"
 // import { Socket } from 'socket.io'
-// import { useNavigate } from 'react-router-dom'
 
-export default function DispayUsers({recentUser}, {setRecentUser}){
+export default function DispayUsers({setCurrentChat}){
     // const socket = useRef()
-    // const navigate = useNavigate()
-    const [contacts, setContacts] = useState([])
-    const [currentUser, setCurrentUser] = useState(undefined)
-    const [online, setOnline] = useState(false)
-    const [currentChat, setCurrentChat] = useState(undefined)
-
-    const token = localStorage.getItem("token")
+    const [contacts, setContacts] = useState([]),
+          [currentUser, setCurrentUser] = useState(""),
+          [online, setOnline] = useState(false)
+    
+    const token = localStorage.getItem("token"),
+          currentUserId = localStorage.getItem("userId")
 
     useEffect(()=>{
         setCurrentUser(localStorage.getItem("userName"))
         !currentUser ? setOnline(true) : setOnline(false)
     }, [token]);
-    console.log(`${currentUser} voici le user en ligne`);
-    console.log(`${online} voici le status en ligne`);
+    // console.log(`${currentUser} voici le user en ligne`);
+    // console.log(`${online} voici le status en ligne`);
 
     // useEffect(()=>{
     //     if(currentUser){
@@ -34,7 +32,7 @@ export default function DispayUsers({recentUser}, {setRecentUser}){
     // },[currentUser])
     
     useEffect(()=>{
-        axios.get(signUpRoute)
+        axios.get(`${signUpRoute}/${currentUserId}`)
         .then((response)=>{
             setContacts(response.data)
             console.log(" les contacts",response.data);
@@ -42,7 +40,7 @@ export default function DispayUsers({recentUser}, {setRecentUser}){
         .catch((err)=>{
             console.log(err);
         })
-    },[])
+    },[token])
 
     return(
         <div  className="usersPage">
@@ -54,19 +52,19 @@ export default function DispayUsers({recentUser}, {setRecentUser}){
           <div className='containerAllUser'>
                 <h3>Recent</h3>
             <div className='AllUser' >
-                {contacts.map((user, index)=>(
-                   <div  className='everyUser' key = {index} >
+                {contacts.map((contact, index)=>(
+                   <div 
+                   onClick={()=>setCurrentChat({
+                    userName: contact.userName,
+                    userId : contact._id
+                   })}
+                   className='everyUser' key = {index} >
                         <img 
                        src={photo} 
                        alt="l'utilisateur" />
-                       <ul   
-                       onClick={()=> setRecentUser({
-                    userName:user.userName,
-                    message : user.userLastName
-                   })} 
-                   >
-                            <li className='userName' >{user.userName}</li>
-                            <li className='recentMessage' >{user.email}</li>
+                       <ul>
+                            <li className='userName' >{contact.userName}</li>
+                            {/* <li className='recentMessage' >{contact._id}</li> */}
                        </ul>
                    </div>  
                 ))}
